35929bdcdd40afb929ee3c5e4d23fc663f3c2b83
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-09 10:58:27 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,87 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private Long id;
+    private  String name;
+    private String email;
+    private String phone;
+    private String passWord;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String passWord) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.passWord = passWord;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", passWord='" + passWord + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "maria", "maria@gmail.com", "8798798", "12321");
+        return ResponseEntity.ok().body(u);
+    }
+}
