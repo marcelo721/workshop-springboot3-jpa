acb44be07a4d86bcb3c1eec8c82bd5ee21d08f1a
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-09 16:06:55 -0300

    category

--- src/main/java/com/educandoweb/course/resources/CategoryResources.java
+++ src/main/java/com/educandoweb/course/resources/CategoryResources.java
@@ -1,7 +1,8 @@
 package com.educandoweb.course.resources;
 
-import com.educandoweb.course.entities.Order;
-import com.educandoweb.course.services.OrderService;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,25 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/orders")
-public class OrderResources {
+@RequestMapping(value = "/categories")
+public class CategoryResources {
 
     @Autowired
-    private OrderService service;
+    private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Order>> findAll(){
-
-        List<Order> list = service.findAll();
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Order> findById(@PathVariable Long id){
+    public ResponseEntity<Category> findById(@PathVariable Long id){
 
-        Order obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        Category category = service.findById(id);
+        return ResponseEntity.ok().body(category);
     }
-
-
 }
